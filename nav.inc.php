b4769a38910bdaa78eaebf3dc5a5a4b0bc2d2bef
jurgen-tan-git <56992789+jurgen-tan-git@users.noreply.github.com>
2023-03-15 10:06:06 +0800

    Refactoring of directory files in the repo

--- nav.inc.php
+++ nav.inc.php
@@ -17,14 +17,13 @@ if(isset($_SESSION["fname"])) {
   <div class="StreamsHero-content">
     <div class="StreamsHero-buttonContainer">
       <a href="https://twitter.com/fredsrocha" class="js-nav" data-element="logo" target="_blank">
-        <i class="fa fa-twitter fa-2x" aria-hidden="true"></i><!--
---><small></small></a>
+        <i class="fa fa-twitter fa-2x" aria-hidden="true"></i><!----><small></small></a>
       
       <a class="Button StreamsSignUp js-signup" href="index.php">Home 
       <a class="Button StreamsLogin js-login"href='login.php'>Memberships</a>
       <a class="Button StreamsSignUp js-signup" href='register.php'>Services 
-      <a class="Button StreamsLogin js-login"href='login.php'>Account Details</a>
-      <a class="Button StreamsSignUp js-signup" href='register.php'>About Us</a>
+      <a class="Button StreamsLogin js-login"href='account.php'>Account Details</a>
+      <a class="Button StreamsSignUp js-signup" href='Aboutus.php'>About Us</a>
 
       <?php echo $content2;?>
 
