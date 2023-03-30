f1dea411f8510eae155ac5562eb03a1625877ce4
KevinthePotatoghost <kelvinneo96@gmail.com>
2023-03-30 19:21:15 +0800

    updated map

--- about_us.php
+++ about_us.php
@@ -51,7 +51,7 @@
                                         <img src="images/SIT.jpg" alt="Main-Building" style="width: 70%">
                                         <figcaption>NYP@SIT</figcaption>
                                     </figure>
-                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.665403029679!2d103.84659831453857!3d1.3774333989953997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da16e96db0a1ab%3A0x3d0be54fbbd6e1cd!2sSingapore%20Institute%20of%20Technology%20(SIT%40NYP)!5e0!3m2!1sen!2ssg!4v1678260741181!5m2!1sen!2ssg" width="400" height="250" style="border:0;" allowfullscreen="" loading="eager" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.665403029679!2d103.84659831453857!3d1.3774333989953997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da16e96db0a1ab%3A0x3d0be54fbbd6e1cd!2sSingapore%20Institute%20of%20Technology%20(SIT%40NYP)!5e0!3m2!1sen!2ssg!4v1678260741181!5m2!1sen!2ssg" class="map" style="border:0; width: 70%"  loading="eager" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 </div>
                             </div>
                         </div>
