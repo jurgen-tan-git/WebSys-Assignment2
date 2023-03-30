49c7aaf914434cc1e4e1ce60208ed9569c11fbbd
jurgen-tan-git <56992789+jurgen-tan-git@users.noreply.github.com>
2023-03-30 18:38:20 +0800

    Changed title

--- login_otp.php
+++ login_otp.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>World of Pets</title>
+    <title>Login OTP</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
